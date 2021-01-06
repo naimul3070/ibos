ca9816b70abc90e97a13ea498c950aeb2e01b914
naimul3070 <16103070@iubat.edu>
2021-01-06 15:40:46 +0600

    This is The first Commit

---
+++ BookApiProject/Iconfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace BookApiProject
+{
+    public class Iconfiguration
+    {
+    }
+}
---
+++ BookApiProject/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using BookApiProject.Services;
+
+namespace BookApiProject
+{
+    public class Startup
+    {
+        public static IConfiguration _configuration { get; set; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var connectionString = _configuration["ConnectionStrings:bookDbConnectionString"];
+            services.AddDbContext<BookDbContext>(c => c.UseSqlServer(connectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,BookDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+            //context.SeedDataContext();
+
+            app.UseMvc();
+        }
+    }
+}
